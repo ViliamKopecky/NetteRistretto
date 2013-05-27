134b933ad15ebba5e98f36c71da59dc476f9341f
ViliamKopecky <vk@viliamkopecky.cz>
2013-05-27 14:40:21 +0200

    Example Gruntfile.js and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    esteWatch: {
+      options: {
+        livereload: { enabled: false  },
+        dirs: [
+          'app',
+          'app/**',
+          'www',
+          'www/**'
+        ]
+      },
+      less: function(filepath) {
+        return ['less'];
+      },
+      css: function(filepath) {
+        return ['ristretto:reloadStyles'];
+      },
+      "latte": function(filepath) {
+        return ['ristretto:reload'];
+      },
+      "php": function(filepath) {
+        return ['ristretto:reload'];
+      },
+      "*": function(filepath) {
+        return ['ristretto:reload'];
+      }
+    },
+    ristretto: {
+      options: {
+        port: 2014
+      }
+    },
+    less: {
+      production: {
+        options: { yuicompress: true  },
+        files: { 'www/css/screen.css': 'www/less/screen.less' }
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-ristretto');
+  grunt.loadNpmTasks('grunt-este-watch');
+
+  grunt.registerTask('default', ['ristretto', 'ristretto:reload', 'esteWatch']);
+};
